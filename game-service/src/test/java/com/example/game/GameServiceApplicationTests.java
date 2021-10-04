63220bfb5055aae1afc360986966b2bfedbc469c
alishhh <alisherkuttybai@gmail.com>
2021-10-05 03:50:44 +0600

    fix

--- game-service/src/test/java/com/example/game/GameServiceApplicationTests.java
+++ game-service/src/test/java/com/example/game/GameServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GameServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
