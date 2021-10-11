33d9f4611f553df867a383d395fe70d214cb2010
Utebayeva <73108508+Utebayeva@users.noreply.github.com>
2021-10-12 02:13:21 +0600

    Add files via upload

--- game-service/src/main/java/com/example/game/entity/Game.java
+++ game-service/src/main/java/com/example/game/entity/Game.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class Game {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long gameId;
     private String title;
     private Double price;
