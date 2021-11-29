61083a4375c3a07c412b0393c8ba7f68f4ee4233
Utebayeva <73108508+Utebayeva@users.noreply.github.com>
2021-11-29 07:00:53 +0600

    update zuul

--- zuul-service/src/main/java/com/example/zuulservice/ZuulServiceApplication.java
+++ zuul-service/src/main/java/com/example/zuulservice/ZuulServiceApplication.java
@@ -1,13 +1,23 @@
 package com.example.zuulservice;
 
+import com.example.zuulservice.config.Filter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class ZuulServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServiceApplication.class, args);
 	}
 
+	@Bean
+	public Filter simpleFilter() {
+		return new Filter();
+	}
 }
