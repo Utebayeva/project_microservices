1480cf33be5079862e4ce330377dac0ef85058a6
Utebayeva <73108508+Utebayeva@users.noreply.github.com>
2021-12-03 23:37:00 +0600

    fix

--- game-service/src/main/java/com/example/game/GameServiceApplication.java
+++ game-service/src/main/java/com/example/game/GameServiceApplication.java
@@ -3,10 +3,14 @@ package com.example.game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 public class GameServiceApplication {
 
     public static void main(String[] args) {
