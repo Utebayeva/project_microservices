88992c065d0b468d567cce4e846809ea489317cd
alishhh <alisherkuttybai@gmail.com>
2021-12-04 00:17:09 +0600

    fix

--- frontend-service/src/main/java/com/example/frontendservice/FrontendServiceApplication.java
+++ frontend-service/src/main/java/com/example/frontendservice/FrontendServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.frontendservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 public class FrontendServiceApplication {
 
 	public static void main(String[] args) {
